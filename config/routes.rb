5392a349349ef1d7fdfaf05cc26a0593d0a7ccbe
hoang anh <hoanganhle30081995@gmail.com>
2021-11-11 20:49:18 +0700

    sign up done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get '/signup' ,to:'user#new'
+  get '/signup' ,to:'users#new'
   get '/home' ,to:'static_pages#home'
   get '/help' ,to:'static_pages#help'
   get '/contact' ,to:'static_pages#contact'
   root "application#hello"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
 end
+

98e076fde2f14b87de5b511e9c5f24eec0198a21
hoang anh <hoanganhle30081995@gmail.com>
2021-11-19 18:53:45 +0700

    chap 10 done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :index, :show, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   def new
     @user = User.new
   end
@@ -13,10 +17,33 @@ class UsersController < ApplicationController
       render "new"
     end
   end
+  
   def show
-    @user = User.find_by(id: params[:id])
+    
+  end
 
-    redirect_to root_path unless @user
+  def edit
+
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
   private
@@ -25,4 +52,26 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    def find_user
+      @user = User.find_by(id: params[:id])
+
+      redirect_to root_path unless @user
+    end
+
+    def logged_in_user
+      unless logged_in?
+      store_location
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
+  end
 end
